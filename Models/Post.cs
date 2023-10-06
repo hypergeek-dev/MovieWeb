6138ba499126e0dbe7dc55b34366da03d51141e6
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-06 13:59:48 +0200

    Add project files.

---
+++ Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System.Reflection;
+
+namespace MovieWeb.Models
+{
+    public class Post
+    {
+     
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string PostImage {  get; set; }
+    }
+}
